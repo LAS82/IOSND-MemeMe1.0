b791f7a8b43cc9c80b592670d89008d8a7ed8faa
Leandro Alves Santos <las@leandros-macbook-air.local>
2018-04-30 19:08:13 -0300

    Empty Project

---
+++ IOSND-MemeMe1.0/IOSND-MemeMe1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSND-MemeMe1.0
+//
+//  Created by Leandro Alves Santos on 30/04/18.
+//  Copyright © 2018 Leandro Alves Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
